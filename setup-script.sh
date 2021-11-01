417168c13293774f1e714cfdd67a890232ad360a
Asim Javed Butt <abutt@redhat.com>
2021-11-01 14:27:22 -0400

    update managedclusters in config

--- setup-script.sh
+++ setup-script.sh
@@ -3,4 +3,4 @@
 export CYPRESS_RESOURCE_ID="${CYPRESS_RESOURCE_ID:-"$(date +"%s")"}"
 
 oc login --insecure-skip-tls-verify -u $CYPRESS_OC_CLUSTER_USER -p $CYPRESS_OC_HUB_CLUSTER_PASS $CYPRESS_OC_HUB_CLUSTER_URL 
-oc get managedclusters $CLUSTER_LABEL_SELECTOR -o custom-columns='name:.metadata.name,available:.status.conditions[?(@.reason=="ManagedClusterAvailable")].status,vendor:.metadata.labels.vendor' --no-headers | awk '/True/ { printf "%s:\n  vendor: %s\n", $1, $3 }' > ./tests/cypress/config/clusters.yaml
+oc get managedclusters $CLUSTER_LABEL_SELECTOR -o custom-columns='name:.metadata.name,available:.status.conditions[?(@.reason=="ManagedClusterAvailable")].status,vendor:.metadata.labels.vendor' --no-headers |grep -i openshift |awk '/True/ { printf "%s:\n  vendor: %s\n", $1, $3 }' > ./tests/cypress/config/clusters.yaml
